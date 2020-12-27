ec773d02412120f3eb2acdd2cbe9e2eb68c536fc
beet <63141491+beetcb@users.noreply.github.com>
2020-12-27 14:56:44 +0800

    feat: a sample to send output as email

---
+++ send.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const NODE = '/usr/bin/node'
+const SIGNDIR = '/home/beet/repo/campushpere-auth-simplified/index.js'
+const MAILDIR = '/home/beet/repo/outlook-simple-config/sendMail'
+
+const { spawnSync } = require('child_process')
+const { relative } = require('path')
+
+const sendMail = require(relative('/home/beet/bin', MAILDIR))
+
+;(() => {
+  const sign = spawnSync(NODE, [SIGNDIR], {
+    encoding: 'utf-8',
+  })
+  const message = sign.stdout || sign.error
+  console.log(sign)
+  // sendMail('☘️ Sign in result ☘️', message.replace(/\n/g, '<br>'))
+})()
